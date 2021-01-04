8feef57c2b6f41f0c03dd0bec6cdf226f8e4b728
Amjad Dardouk <47476144+AmjadDardouk@users.noreply.github.com>
2021-01-04 23:46:41 +0200

    Update Create.cs

--- task/Create.cs
+++ task/Create.cs
@@ -6,6 +6,7 @@ namespace task
     public class Create
     {
         public static DbContext _context = new DbContext();
+        DateTime currentDate = DateTime.Now;
 
         public Create()
         {
@@ -17,7 +18,7 @@ namespace task
         }
         public void CreateNewUser(string first_name, string last_name, string email, string password)
         {
-            User new_user = new User { Email = email, FirstName = first_name, LastName = last_name, Password = password };
+            User new_user = new User { Email = email, FirstName = first_name, LastName = last_name, Password = password ,Created = currentDate,Updated = currentDate};
             _context.Users.Add(new_user);
         }
 
