fbcb0f7b733d5a7b416a095391fc1902200ce6d8
Amjad Dardouk <47476144+AmjadDardouk@users.noreply.github.com>
2021-01-04 22:17:45 +0200

    Update Create.cs

--- task/Create.cs
+++ task/Create.cs
@@ -13,8 +13,7 @@ namespace task
 
         public Create(string first_name, string last_name, string email, string password)
         {
-            User new_user = new User { Email = email, FirstName = first_name, LastName = last_name, Password = password };
-            _context.Users.Add(new_user);
+            CreateNewUser(first_name, last_name, email, password);
         }
         public void CreateNewUser(string first_name, string last_name, string email, string password)
         {
